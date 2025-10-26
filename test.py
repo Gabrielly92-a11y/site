bc1f64709544cb854465ca55c234271e8e48db54
Gabrielly92-a11y <gabrielly.qrz@outlook.com>
2025-10-26 02:28:28 -0300

    initial commit

--- test.py
+++ test.py
@@ -1,39 +1,22 @@
-import time 
+from qistik  import QuantumCircuit, transpile
+from qistik_aer import AerSimulator
+from qistik.visualization import plot_histogram
 
-letra_musica = """"
-    imagine there´s no heaven
-    it´s easy if you try
-    no hell below us
-    above us, only sky
-    imagine all the people
-    living for today....aha-ah..
-    
-    imagine there´s no countries
-    it isn´t hard to do
-    nothing to kill or die for
-    and no religion, too
-    imagine all the people
-    living life in peace... you...
-    
-    you may say i´m a dreamer
-    but i´m not the only one
-    i hope someday you´ll join us
-    and the world will be as one
-    
-    imagine no possessions
-    i wonder if you can
-    no need for greed or hunger
-    a brotherhood of man
-    imagine all the people
-    sharing all the world... you..
-    
-    you may say i´m a dreamer
-    but i´m not the only one
-    i hope someday you´ll join us
-    and the world wiil live as one"""
+QuantumCircuit(2, 2) #circuito com 2qubits e 2bits classicos
 
-for linha in letra_musica.split('\n'):
-    print(f"\033{linha}\033[0m")
-    time.sleep(1.5)
+qc.h(0) #Hardamard
 
-print(letra_musica)
+qc.cx(0, 1) #CNOT
+
+qc.measure([0, 1], [0, 1])
+
+simulator = AerSimulator()
+compile_circuit = transpile(qc, simulator)
+job = simulator.run(compile_circuit, shosts=1000)
+
+result = job.result()
+counts = result.get_counts(qc)
+print("\ncontagem dos resultados:", counts)
+
+plot_histogram(counts)
+
